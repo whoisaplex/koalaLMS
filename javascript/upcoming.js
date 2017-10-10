dcb2252f1e5e88b0315b3f92be99c00de7bc4c8f
Aplex <alexander.dahlberg@student.kyh.se>
2017-10-10 15:39:35 +0200

    create new index.css
    create new index.css for index.html

--- javascript/upcoming.js
+++ javascript/upcoming.js
@@ -1,8 +1,6 @@
-let loggedInUser = 'alexander';
-
 const u_length = users.length;
 for(let x = 0; x < u_length; x++){
-	if (users[x].username === loggedInUser && users[x].role === 'student') {
+	if (users[x].username === logged_in_user && users[x].role === 'student') {
 		for(let a = 0; a < users[x].all_upcoming[0].upcoming_exams.length; a++ ){
 			let temp_exam = users[x].all_upcoming[0].upcoming_exams[a];
 			document.getElementById('exams').innerHTML += '<div class="small-box-li"><h4 onclick="toggle_course(this)" style="cursor:pointer">'+ temp_exam.due_date + " - " + temp_exam.name + '</h4><p>' + temp_exam.description + '</p></div>'
