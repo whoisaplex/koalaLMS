ad106e1c72422773fc34519097a54e54fe2fa6f1
Aplex <alexander.dahlberg@student.kyh.se>
2017-10-10 11:22:37 +0200

    add databse to index.html
    add database to index.html

--- javascript/upcoming.js
+++ javascript/upcoming.js
@@ -1,15 +1,15 @@
-let loggedInUser = 'Alexander';
+let loggedInUser = 'alexander';
 
 const u_length = users.length;
 for(let x = 0; x < u_length; x++){
 	if (users[x].username === loggedInUser && users[x].role === 'student') {
 		for(let a = 0; a < users[x].all_upcoming[0].upcoming_exams.length; a++ ){
 			let temp_exam = users[x].all_upcoming[0].upcoming_exams[a];
-			document.getElementById('exams').innerHTML += '<div class="small-box-li"><h4 onclick="toggle_course(this)" style="cursor:pointer">'+ temp_exam.due_date + " - " + temp_exam.name + '</h4><p>' + temp_exam.description + '</p></div>'	
+			document.getElementById('exams').innerHTML += '<div class="small-box-li"><h4 onclick="toggle_course(this)" style="cursor:pointer">'+ temp_exam.due_date + " - " + temp_exam.name + '</h4><p>' + temp_exam.description + '</p></div>'
 		}
 		for(let b = 0; b < users[x].all_upcoming[1].upcoming_assignments.length; b++){
 			let temp_assignment = users[x].all_upcoming[1].upcoming_assignments[b];
-			document.getElementById('assignments').innerHTML += '<div class="small-box-li"><h4 onclick="toggle_course(this)" style="cursor:pointer">'+ temp_assignment.due_date + " - " + temp_assignment.name + '</h4><p>' + temp_assignment.description + '</p></div>'	
+			document.getElementById('assignments').innerHTML += '<div class="small-box-li"><h4 onclick="toggle_course(this)" style="cursor:pointer">'+ temp_assignment.due_date + " - " + temp_assignment.name + '</h4><p>' + temp_assignment.description + '</p></div>'
 		}
 	}
 }
@@ -20,4 +20,3 @@ function toggle_course(current){
     current.parentElement.childNodes[1].style.display = 'none';
   }
 }
-
