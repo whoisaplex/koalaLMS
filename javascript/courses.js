c4048bce823e1a59e0783e7ed0c75a0874f58f1d
Aplex <alexander.dahlberg@student.kyh.se>
2017-10-17 16:09:54 +0200

    Change courses page
    change courses page to multiple pages

--- javascript/courses.js
+++ javascript/courses.js
@@ -1,3 +1,28 @@
+let path = window.location.pathname;
+let page = path.split("/").pop();
+page = page.replace(/[.].*/, "");
+page = page.charAt(0).toUpperCase() + page.slice(1);
+
+
+console.log(page);
+for(let i = 0; i < users.length; i++){
+  if(users[i].username === 'alexander'){
+  for(let z = 0; z < 1; z++){
+      for(let x = 0; x < users[i].all_courses[0].ongoing.length; x++){
+        if(users[i].all_courses[0].ongoing[x].name === page){
+            document.getElementById('hubba').innerHTML = '<h2>'+users[i].all_courses[0].ongoing[x].name+'</h2><h3>Tempo: '+users[i].all_courses[0].ongoing[x].tempo+' Teacher: '+users[i].all_courses[0].ongoing[x].teacher+'</h3><hr><h4>Description</h4><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p><h5><a href="#">Read full description</a></h5>';
+        }
+      }
+      for(let x = 0; x < users[i].all_courses[1].upcoming.length; x++){
+        if(users[i].all_courses[1].upcoming[x].name === page){
+          document.getElementById('hubba').innerHTML = '<h2>'+users[i].all_courses[1].upcoming[x].name+'</h2><h3>Tempo: '+users[i].all_courses[1].upcoming[x].tempo+' Teacher: '+users[i].all_courses[1].upcoming[x].teacher+'</h3><hr><h4>Description</h4><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p><h5><a href="#">Read full description</a></h5>';
+        }
+      }
+    }
+  }
+}
+
+/*
 const u_lengthh = users.length;
 for(let x = 0; x < u_lengthh; x++){
   if(users[x].username === logged_in_user && users[x].role === 'student'){
@@ -18,7 +43,7 @@ for(let x = 0; x < u_lengthh; x++){
     }
   }
 }
-
+*/
 
 
 //Shows/Hides course content on click
--- javascript/nav.js
+++ javascript/nav.js
@@ -11,16 +11,54 @@ for(let x = 0; x < users.length; x++){
 document.getElementById('user-Name').innerHTML = users[num].full_name;
 */
 
+function toggleCourses(){
+  if(document.getElementById('nav-courses').style.display === 'none'){
+    document.getElementById('nav-courses').style.display = 'block';
+  }else{
+    document.getElementById('nav-courses').style.display = 'none';
+  }
+}
+
+function toggleOngoing(){
+  if(document.getElementById('nav-ongoing-courses').style.display === 'none'){
+    document.getElementById('nav-ongoing-courses').style.display = 'block';
+    document.getElementById('nav-ongoing-courses-arrow').classList = 'fa fa-chevron-down';
+    document.getElementById('nav-ongoing-courses-text').style.background = '#2E3344';
+  }else{
+    document.getElementById('nav-ongoing-courses').style.display = 'none';
+    document.getElementById('nav-ongoing-courses-arrow').classList = 'fa fa-chevron-right';
+    document.getElementById('nav-ongoing-courses-text').style.background = 'none';
+  }
+}
+
+function toggleUpcoming(){
+  if(document.getElementById('nav-upcoming-courses').style.display === 'none'){
+    document.getElementById('nav-upcoming-courses').style.display = 'block';
+    document.getElementById('nav-upcoming-courses-arrow').classList = 'fa fa-chevron-down';
+    document.getElementById('nav-upcoming-courses-text').style.background = '#2E3344';
+  }else{
+    document.getElementById('nav-upcoming-courses').style.display = 'none';
+    document.getElementById('nav-upcoming-courses-arrow').classList = 'fa fa-chevron-right';
+    document.getElementById('nav-upcoming-courses-arrow').style.background = '';
+    document.getElementById('nav-upcoming-courses-text').style.background = 'none';
+  }
+}
+
+
+
+
+
 let pages = ['Home', 'Courses','Upcoming', 'Grades'];
 for(let countPages = 0; countPages < pages.length; countPages++){
   if(pages[countPages] === currentPage){
-    document.getElementById(currentPage).style.display = 'grid';
-    document.getElementById('nav-menu').childNodes[1].style.background = '#2e3344';
-    document.getElementById('nav-menu').childNodes[1].style.borderRight = 'solid 4px orange';
+    //document.getElementById(currentPage).style.display = 'grid';
+    //document.getElementById('nav-menu').childNodes[1].style.background = '#2e3344';
+    //document.getElementById('nav-menu').childNodes[1].style.borderRight = 'solid 4px orange';
   }else{
-    document.getElementById(pages[countPages]).style.display = 'none';
+    //document.getElementById(pages[countPages]).style.display = 'none';
   }
 }
+
 
 
 function changeContent(currentNavP){
