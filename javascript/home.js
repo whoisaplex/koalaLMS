99f4846dd2c8f9229a56be9bf651d71bc0b0d26a
Aplex <alexander.dahlberg@student.kyh.se>
2017-10-18 13:58:14 +0200

    confirm attendance to classes
    confirm attendance to classes with code

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,5 @@
+let currentAttendanceBox;
+
 if(innerWidth <= 769){
   document.getElementById('Home').style.display = 'none';
   document.getElementById('nav-wrapper').style.display = 'none';
@@ -5,5 +7,32 @@ if(innerWidth <= 769){
   document.getElementById('Home-mobile').style.display = 'none';
 }
 function changeAttendance(currentAttendance){
-  currentAttendance.parentElement.childNodes[1].src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/checkmark-24-256.png';
+  if(document.getElementById('attendance-dropdown').style.display === 'none' || document.getElementById('attendance-dropdown').style.display === ''){
+    document.getElementById('attendance-dropdown').style.display = 'block';
+    document.getElementById('attendance-dark-overlay').style.display = 'block';
+    currentAttendanceBox = currentAttendance;
+  }else{
+    document.getElementById('attendance-dropdown').style.display = 'none';
+  }
+}
+
+function submitCodeAttendance(){
+  let code = "222EEE";
+  let codeInput = document.getElementById('attendance-dropdown-input').value;
+  if(codeInput === code){
+    currentAttendanceBox.parentElement.childNodes[1].src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/checkmark-24-256.png';
+    document.getElementById('attendance-dropdown').style.display = 'none';
+    document.getElementById('attendance-dark-overlay').style.display = 'none';
+    document.getElementById('attendance-dropdown-input').value = '';
+  }else{
+    document.getElementById('attendance-dropdown-error').style.display = 'grid';
+  }
+}
+
+function closeAttendanceDropdown(){
+  document.getElementById('attendance-dropdown').style.display = 'none';
+  document.getElementById('attendance-dark-overlay').style.display = 'none';
+  document.getElementById('attendance-dropdown-error').style.display = 'none';
+  document.getElementById('attendance-dropdown-input').value = '';
+
 }
