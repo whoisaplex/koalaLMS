e3fd6321f538128135b470e39f98ee32447d474c
masudulhas <masudul.has@gmail.com>
2017-10-19 12:32:28 +0200

    changes to style.css,database.js,grades.js for grades.html page

--- javascript/grades.js
+++ javascript/grades.js
@@ -6,9 +6,9 @@ if(users[x].username === loggedinUser && users[x].role === 'student'){
    //Adding finished courses to the html
         for(let c= 0; c < users[x].all_finished_courses[0].finished_courses.length; c++){
      let temp_course = users[x].all_finished_courses[0].finished_courses[c];
-     document.getElementById('finished-coursess').innerHTML += '<div><h4>'+temp_course.name
-      +'</h4><div><p>Avarage grade: '+temp_course.average_grade+'</p><p>Tempo: '+temp_course.tempo+'</p><p>Assignment1: '+temp_course.assignment1+'</p><p>Grade: '+temp_course.grade1+'</p><p>Assignment2: '+temp_course.assignment2+'</p><p>Grade: '+temp_course.grade2+'</p><p>Assignment3: '
-      +temp_course.assignment3+'</p><p>Grade: '+temp_course.grade3+'</p></div></div>';
+     document.getElementById('finished-coursess').innerHTML += '<h2>'+temp_course.name
+      +'</h2><h3>Avarage grade: '+temp_course.average_grade+'<p>Tempo: '+temp_course.tempo+'</p></h3><hr><p>Assignment: '+temp_course.assignment1+'</p><p>Grade: '+temp_course.grade1+'</p><p>Assignment: '+temp_course.assignment2+'</p><p>Grade: '+temp_course.grade2+'</p><p>Assignment: '
+      +temp_course.assignment3+'</p><p>Grade: '+temp_course.grade3+'</p>';
    }
  }
 }
