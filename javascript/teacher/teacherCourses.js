52ff80358ff3e37e11ef4eceafff7ca36d6339ed
Aplex <alexander.dahlberg@student.kyh.se>
2017-10-23 13:22:42 +0200

    delete assignments and examinations bug fix
    bug fix for delete button

--- javascript/teacher/teacherCourses.js
+++ javascript/teacher/teacherCourses.js
@@ -6,33 +6,32 @@ for(let x = 0; x < teachers.length; x++){
   for(let z = 0; z < teachers[x].courses.length; z++){
     if(teachers[x].courses[z].name === pageCourses){
       for(let y = 0; y < teachers[x].courses[z].assignments.length; y++){
-        document.getElementById('teacherAssignmentsBox').innerHTML += '<div><p class="teacherAssignmentName">'+teachers[x].courses[z].assignments[y].name+'</p><p class="teacherAssignmentEdit" onclick="editAssignment(this)"><i class="fa fa-pencil-square-o"></i></p><p class="teacherAssignmentTrash" onclick="deleteAssignment(this)"><i class="fa fa-trash-o"></i></p></div>';
+        document.getElementById('teacherAssignmentsBox').innerHTML += '<div id="12"><p class="teacherAssignmentName">'+teachers[x].courses[z].assignments[y].name+'</p><p class="teacherAssignmentEdit" onclick="editAssignment(this)"><i class="fa fa-pencil-square-o"></i></p><p class="teacherAssignmentTrash" onclick="deleteAssignment(this)"><i class="fa fa-trash-o"></i></p></div>';
       }
       for(let y = 0; y < teachers[x].courses[z].examinations.length; y++){
-        document.getElementById('teacherExaminationsBox').innerHTML += '<div><p class="teacherAssignmentName">'+teachers[x].courses[z].examinations[y].name+'</p><p class="teacherAssignmentEdit" onclick="editAssignment(this)"><i class="fa fa-pencil-square-o"></i></p><p class="teacherAssignmentTrash" onclick="deleteAssignment(this)"><i class="fa fa-trash-o"></i></p></div>';
+        document.getElementById('teacherExaminationsBox').innerHTML += '<div id="13"><p class="teacherAssignmentName">'+teachers[x].courses[z].examinations[y].name+'</p><p class="teacherAssignmentEdit" onclick="editAssignment(this)"><i class="fa fa-pencil-square-o"></i></p><p class="teacherAssignmentTrash" onclick="deleteAssignment(this)"><i class="fa fa-trash-o"></i></p></div>';
       }
     }
   }
 }
 
 
-
-
 function editAssignment(currentAssignment){
   currentAssignment.parentNode.childNodes[0].innerHTML = 'HTML5';
 }
+
 function deleteAssignment(currentAssignment){
   document.getElementById('teacher-dark-overlay').style.display = 'block';
   document.getElementById('teacher-delete-dropdown').style.display = 'block';
-  document.getElementById('delete').addEventListener("click", function(){
+  document.getElementById('delete').onclick = function(){
     currentAssignment.parentNode.remove();
     document.getElementById('teacher-dark-overlay').style.display = 'none';
     document.getElementById('teacher-delete-dropdown').style.display = 'none';
-  });
-  document.getElementById('cancel').addEventListener("click", function(){
+  };
+  document.getElementById('cancel').onclick = function(){
     document.getElementById('teacher-dark-overlay').style.display = 'none';
     document.getElementById('teacher-delete-dropdown').style.display = 'none';
-  });
+  };
 }
 
 function addAssignment(){
