3c458219efc646473b23379da227e22ff9109e58
liolios <simon.liolios@hyperisland.se>
2017-10-30 13:32:21 +0100

    add charts.js for teacher 'home'

--- javascript/login.js
+++ javascript/login.js
@@ -24,10 +24,10 @@ function populateStorage(loginUserName) {
 
 function loginFunction(){
     
-    var loginUserName    = document.getElementById("login-userName").value;
-    var loginPsw         = document.getElementById("login-psw").value;
+    let loginUserName    = document.getElementById("login-userName").value;
+    let loginPsw         = document.getElementById("login-psw").value;
    
-    for(var i=0; i< students.length; i++){
+    for(let i=0; i< students.length; i++){
        if(students[i].username === loginUserName && students[i].password === loginPsw ){
             sessionStorage.setItem('loginName', JSON.stringify(userName));
            if(populateStorage(loginUserName)){
@@ -37,7 +37,6 @@ function loginFunction(){
                document.getElementById('register-btn').style.display='none';
                document.getElementById('login-btn').style.display='none';
                document.getElementById('userLogin').style.display='none';
-
            }
        } else {
            for(let y=0; y< teachers.length; y++){
@@ -50,15 +49,13 @@ function loginFunction(){
                  document.getElementById('register-btn').style.display='none';
                  document.getElementById('login-btn').style.display='none';
                  document.getElementById('userLogin').style.display='none';
-           }
-      
+            }
            } else {
             document.getElementById('login-error').innerHTML = "<span class='login-error info-alert warning-info-alert'>User Name / Password is not valid</span>";
            }
-
        }
      }
-}
+  }
 }
 
 
