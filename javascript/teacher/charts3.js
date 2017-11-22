c5731c37288757aeac562da809efb09eef2eb76f
liolios <simon.liolios@hyperisland.se>
2017-11-22 10:49:30 +0100

    quickfix chart

--- javascript/teacher/charts3.js
+++ javascript/teacher/charts3.js
@@ -3,10 +3,10 @@ window.onload = function () {
     var myDailyChart = new Chart(ctx3, {
     type: 'bar',
     data: {
-        labels: ["Mon", "Tues", "Wedn", "Thur", "Fri", "Sat", "Sun"],
+        labels: ["Monday", "Tuesday", "Wednessday", "Thursday", "Friday"],
         datasets: [{
             label: 'days',
-            data: [4, 5, 6, 7, 8, 9, 10],
+            data: [3, 5, 6, 7, 10],
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 2
