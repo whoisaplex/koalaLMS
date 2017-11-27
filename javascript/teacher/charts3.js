222c1fa195375ddbe14901ef1b004c4f1d31548c
liolios <simon.liolios@hyperisland.se>
2017-11-27 16:51:27 +0100

    fix account.html edit bug

--- javascript/teacher/charts3.js
+++ javascript/teacher/charts3.js
@@ -4,17 +4,26 @@
     data: {
         labels: ["Monday", "Tuesday", "Wednessday", "Thursday", "Friday"],
         datasets: [{
-            label: 'days',
+            label: ':)',
             data: [3, 5, 6, 7, 10],
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 2
-        }]
+        },
+        {
+            label: ':|',
+            data: [3,4,5,6,7,8],
+            backgroundColor: 'blue',
+        },
+        {
+            label: ':(',
+            data: [3,4,5,6,7,8],
+            backgroundColor: 'blue',
+        }
+
+        ]
     },
     options: {
-         legend: {
-            display: false
-         },
     scales: {
         yAxes: [{
             ticks: {
