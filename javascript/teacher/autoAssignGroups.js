333462d4cc52bfc5a3a201d84658ded4f3f62cc3
Aplex <alexander.dahlberg@student.kyh.se>
2017-11-28 08:53:15 +0100

    delete not used code

--- javascript/teacher/autoAssignGroups.js
+++ javascript/teacher/autoAssignGroups.js
@@ -1,13 +1,6 @@
 function makeGroup(){
 
   let studenters = document.getElementById('studentHolder').children;
-
-  /*let varr = document.getElementById('studentHolder').children[0];
-  varr.remove();
-  memes = document.getElementById('studentHolder').children.length;
-  console.log(memes);
-*/
-
   let groups = 5;
   let group = [];
   if(studenters.length % groups){
@@ -22,13 +15,10 @@ function makeGroup(){
       tempDiv.innerHTML += '<p>Group ' + (i+1) + ':</p>';
       for(let y = 0; y < peoplePerGroup; y++){
         tempDiv.children[0].innerHTML += ' ' + studenters[0].children[0].innerHTML;
-        //TempGroup.push(studenters[0]);
         studenters[0].remove();
         console.log(tempDiv);
       }
       document.getElementById('groupHolder').appendChild(tempDiv);
-      //tempDiv.appendChild('groupHolder');
-      //group[i] = (TempGroup);
     }
     for(let z = 0; z < extras; z++){
       document.getElementById('group' + z).children[0].innerHTML += ' ' + studenters[0].children[0].innerHTML;
