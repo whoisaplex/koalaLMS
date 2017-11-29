c347fe67e2588592bbeec671738f7036c7258cf6
liolios <simon.liolios@hyperisland.se>
2017-11-29 10:23:18 +0100

    fix chart colors

--- javascript/teacher/charts3.js
+++ javascript/teacher/charts3.js
@@ -6,19 +6,23 @@
         datasets: [{
             label: ':)',
             data: [3, 5, 6, 7, 10],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255,99,132,1)',
+            backgroundColor: 'rgba(58, 249, 44,0.2)',
+            borderColor: 'rgba(58, 249, 44,1)',
             borderWidth: 2
         },
         {
             label: ':|',
             data: [3,4,5,6,7,8],
-            backgroundColor: 'blue',
+            backgroundColor: 'rgba(249, 255, 76, 0.4)',
+            borderColor: 'rgba(249, 255, 76, 1)',
+            borderWidth: 2
         },
         {
             label: ':(',
-            data: [3,4,5,6,7,8],
-            backgroundColor: 'blue',
+            data: [7,4,5,6,7,8],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 2
         }
 
         ]
