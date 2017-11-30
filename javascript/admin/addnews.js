f7d8c8cbcd0f773be2c717ce3bc5304047b7d6b4
Oskar Ray Frayssinet <oskar.rf@live.se>
2017-11-30 15:23:57 +0100

    add ikons and function in admin news

--- javascript/admin/addnews.js
+++ javascript/admin/addnews.js
@@ -15,21 +15,44 @@ function addNews(){
         setTimeout(function(){text.style.animation = ""}, 800);}
     if(title.value && author.value && text.value){
         let newNews = document.createElement("div");//"<div class='news-box' onclick='editNews(this)'> <i class='fa fa-wrench' aria-hidden='true'></i>updating website 24/7</div>"; 
-        let iClass = document.createElement("i");
         let pClass = document.createElement("p");
+        let pClassEdit = document.createElement("p");
+        let pClassTrash = document.createElement("p");
+        let iClassEdit = document.createElement("i");
+        let iClassTrash = document.createElement("i");
 
         pClass.innerHTML = title.value;
-        iClass.setAttribute("class","fa fa-wrench");
-        iClass.setAttribute("aria-hidden", "true");
+        pClassEdit.setAttribute("class", "adminNewsEdit");
+        pClassTrash.setAttribute("class", "adminNewsTrash");
+
+        iClassEdit.setAttribute("class","fa fa-pencil-square-o");
+        iClassEdit.setAttribute("aria-hidden", "true");
+        iClassTrash.setAttribute("class","fa fa-trash-o");
+        iClassTrash.setAttribute("aria-hidden", "true");
         pClass.setAttribute("class", "titleNews");
 
+        pClassEdit.appendChild(iClassEdit);
+        pClassTrash.appendChild(iClassTrash);
+        pClassTrash.setAttribute("onclick", "trashNews(this)");
+
         newNews.className = "news-box";
-        newNews.appendChild(iClass);//"<i class='fa fa-wrench' aria-hidden='true'></i>";
         newNews.appendChild(pClass);
+        newNews.appendChild(pClassEdit);
+        newNews.appendChild(pClassTrash);
         //newNews.appendChild += text.value;
         document.getElementById("current-news").appendChild(newNews);
         title.value = "";
         author.value = "";
         text.value = "";
     }
+}
+function editNews(elementEdit){
+    /*let editTitle = elementEdit
+    let editAuthor = elementEdit
+    let editText = elementEdit*/
+    console.log(elementEdit);
+    elementEdit.style.backgroundColor = "grey";
+}
+function trashNews(elementTrash){
+    elementTrash.parentNode.parentNode.removeChild(elementTrash.parentNode);
 }
