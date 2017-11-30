cddb7be32cd675b717f5414faf6525293ca7715d
Oskar Ray-Frayssinet <oskar.rf@live.se>
2017-11-30 19:04:08 +0100

    admin added news can now be updated

--- javascript/admin/addnews.js
+++ javascript/admin/addnews.js
@@ -16,14 +16,20 @@ function addNews(){
         text.style.animation = "validationFeedback 0.8s ease";
         setTimeout(function(){text.style.animation = ""}, 800);}
     if(title.value && author.value && text.value){
-        let newNews = document.createElement("div");//"<div class='news-box' onclick='editNews(this)'> <i class='fa fa-wrench' aria-hidden='true'></i>updating website 24/7</div>"; 
-        let pClass = document.createElement("p");
+
+        //create DOM elements for new news div
+        let newNews = document.createElement("div");
+        let pClassTitle = document.createElement("p");
+        let pClassAuthor = document.createElement("p");
+        let pClassText = document.createElement("p");
         let pClassEdit = document.createElement("p");
         let pClassTrash = document.createElement("p");
         let iClassEdit = document.createElement("i");
         let iClassTrash = document.createElement("i");
 
-        pClass.innerHTML = title.value;
+        pClassTitle.innerHTML = title.value;
+        pClassAuthor.innerHTML = author.value;
+        pClassText.innerHTML = text.value;
         pClassEdit.setAttribute("class", "adminNewsEdit");
         pClassTrash.setAttribute("class", "adminNewsTrash");
 
@@ -31,17 +37,25 @@ function addNews(){
         iClassEdit.setAttribute("aria-hidden", "true");
         iClassTrash.setAttribute("class","fa fa-trash-o");
         iClassTrash.setAttribute("aria-hidden", "true");
-        pClass.setAttribute("class", "titleNews");
+        pClassTitle.setAttribute("class", "titleNews");
 
+        pClassEdit.setAttribute("onclick", "editAddedNews(this)");
         pClassEdit.appendChild(iClassEdit);
-        pClassTrash.appendChild(iClassTrash);
         pClassTrash.setAttribute("onclick", "trashNews(this)");
+        pClassTrash.appendChild(iClassTrash);
+
+        pClassAuthor.setAttribute("class", "authorNews");
+        pClassText.setAttribute("class", "textNews");
 
         newNews.className = "news-box";
-        newNews.appendChild(pClass);
+        newNews.appendChild(pClassTitle);
+        newNews.appendChild(pClassAuthor);
+        newNews.appendChild(pClassText);
         newNews.appendChild(pClassEdit);
         newNews.appendChild(pClassTrash);
-        //newNews.appendChild += text.value;
+
+        console.log(newNews);
+
         document.getElementById("current-news").appendChild(newNews);
         title.value = "";
         author.value = "";
@@ -59,6 +73,18 @@ function editNews(elementEdit){
     document.getElementById("news-text-input").value = elementEdit.parentNode.childNodes[5].innerHTML;
     newsHolder = elementEdit.parentNode;
 }
+//for news that are added through website because of different child nodes length 
+function editAddedNews(elementEdit){
+    document.getElementById("news-submit-input").value = "Update News";
+    document.getElementById("news-submit-input").removeAttribute("onclick");
+    document.getElementById("news-submit-input").setAttribute("onclick", "updateAddedNews()");
+
+    elementEdit.parentNode.style.backgroundColor = "grey";
+    document.getElementById("news-title-input").value = elementEdit.parentNode.childNodes[0].innerHTML;
+    document.getElementById("news-author-input").value = elementEdit.parentNode.childNodes[1].innerHTML;
+    document.getElementById("news-text-input").value = elementEdit.parentNode.childNodes[2].innerHTML;
+    newsHolder = elementEdit.parentNode;
+}
 function trashNews(elementTrash){
     elementTrash.parentNode.parentNode.removeChild(elementTrash.parentNode);
 }
@@ -72,6 +98,25 @@ function updateNews(newsDiv){
     newsHolder.childNodes[1].innerHTML = document.getElementById("news-title-input").value;
     newsHolder.childNodes[3].innerHTML = document.getElementById("news-author-input").value;
     newsHolder.childNodes[5].innerHTML = document.getElementById("news-text-input").value;
+    console.log(newsHolder);
+
+    //clear "Add News" input boxes
+    document.getElementById("news-title-input").value = "";
+    document.getElementById("news-author-input").value = "";
+    document.getElementById("news-text-input").value = "";
+}
+
+//for news that are added through website because of different child nodes length
+function updateAddedNews(newsDiv){
+    document.getElementById("news-submit-input").value = "Submit";
+    document.getElementById("news-submit-input").setAttribute("onclick", "addNews()");
+    let newsBoxArray = document.getElementsByClassName("news-box");
+    for(let i=0; i < newsBoxArray.length; i++)
+        newsBoxArray[i].style.backgroundColor = "#006ABF";
+
+    newsHolder.childNodes[0].innerHTML = document.getElementById("news-title-input").value;
+    newsHolder.childNodes[1].innerHTML = document.getElementById("news-author-input").value;
+    newsHolder.childNodes[2].innerHTML = document.getElementById("news-text-input").value;
     console.log(newsHolder);
 
     //clear "Add News" input boxes
